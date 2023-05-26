725edfea3175c57f71dced6005823cd348b0ecb1
ArcadeFortune <alessionights@gmail.com>
2023-05-26 23:41:16 +0200

    feat: added loading animation

--- InstantGPT.py
+++ InstantGPT.py
@@ -1,98 +1,132 @@
-import webbrowser
+import os
+import openai
 import keyboard
 import pyautogui
 import pyperclip
-import win32gui
-import win32con
-import os
-from dotenv import load_dotenv
-import openai
+import threading
+import webbrowser
 import tkinter as tk
+from dotenv import load_dotenv
 from markdown2 import Markdown
 from tkinterweb import HtmlFrame
-markdowner = Markdown()
-loadingWindow = tk.Tk()
 
+# OpenAI API key
 load_dotenv()
-
 key = os.getenv('key')
 openai.api_key = key
-previous_text = ""
 
+# Markdown
+markdowner = Markdown()
+
+# Global variables
+previous_text = pyperclip.paste()
+received = False
 
 def instantGPT():
-    global previous_text
-    current_text = readHighlightedText(previous_text)
-    root = tk.Tk()
-    
-
-
-    if current_text: # Error handling
-        if current_text != previous_text:
-            
-            
-            
-            print("sending request...", end="\r")
-            answer = sendRequest(current_text)
-            print("answer: " + answer)
-            converted = markdowner.convert(answer)
-            myhtmlframe = HtmlFrame(root, horizontal_scrollbar="auto")
-            myhtmlframe.load_html(converted)
-            myhtmlframe.pack(fill="both", expand=True)
-
-            root.mainloop()
-            # win32gui.PostMessage(message, win32con.WM_CLOSE, 0, 0)
-        else:
-            # No highlighted text, open OpenAI website
-            print("No highlighted text, open OpenAI website")
-            webbrowser.open('https://chat.openai.com')
-            
-    else: # Error handling
-        win32gui.MessageBox(0, "Something went wrong with your highlighted text...", "Instant GPT", win32con.MB_OK | win32con.MB_SETFOREGROUND)
+  global previous_text
+  global received
+  current_text = readHighlightedText()
+  
+  root = tk.Tk()
+  
+  if current_text: # Error handling
+    if current_text != previous_text:
+      answer = sendRequest(current_text)
+      if answer:          
+        received = True # Confirm that answer has been received
+        converted = markdowner.convert(answer)
+        myhtmlframe = HtmlFrame(root, horizontal_scrollbar="auto", messages_enabled=False)
+        # myhtmlframe.messages_enabled = False
+        myhtmlframe.load_html(converted)
+        myhtmlframe.pack(fill="both", expand=True)
+        root.mainloop()
         
-    previous_text = current_text
-    # global loadingWindow
-    # loadingWindow.destroy()
+      else:
+        print("Something went wrong with the API request")
+        
+    else:
+      # No highlighted text, open OpenAI website
+      print("No highlighted text, open OpenAI website")
+      webbrowser.open('https://chat.openai.com')
+      
+  else: # Error handling
+    print("Something went wrong with the highlighted text")
+    
+  previous_text = current_text # Update previous text
 
+def readHighlightedText():
+  pyautogui.hotkey('ctrl', 'c')    
+  current_text = pyperclip.paste()
+  print("highlighted text: " + current_text)
+  return current_text
 
-def readHighlightedText(previous_text):
-    pyautogui.hotkey('ctrl', 'c')    
-    current_text = pyperclip.paste()
-    print("highlighted text: " + current_text)
-    return current_text
-
-def sendRequest(current_text):
-    answer = ""
+def sendRequest(current_text):  
+  try:      
+    answer = ""    
+    print("sending request...", end="\r")
             
     message=[{"role": "user", "content": current_text}]
     response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=message,
-            )
+      model="gpt-3.5-turbo",
+      messages=message,
+    )
     answer = response['choices'][0]['message']['content']
+    print("returned answer: " + answer)
     return answer
-    
-def loadWindow():
-    print('loading window...')
-    # Create a window 
-    global loadingWindow
-    # Set the window settings
-    loadingWindow.title("Self-Closing Window")
-    # Remove the buttons ontop
-    loadingWindow.overrideredirect(True)
-    # Move the window to the current mouse position
-    loadingWindow.geometry(f"+{loadingWindow.winfo_pointerx()}+{loadingWindow.winfo_pointery()}")
-    # Write text into the window
-    content_label = tk.Label(loadingWindow, text="Loading...", font=("Arial", 22))
-    content_label.pack()
-    
 
-    # Do something while loading the window
-    loadingWindow.after(1, instantGPT)
-    loadingWindow.mainloop()
+  except openai.error.APIError as e:
+    # Handle API errors
+    return 'API Error:', str(e)
 
-# Listen for hotkey
-previous_text = pyperclip.paste()
-keyboard.add_hotkey("ctrl+shift+o", instantGPT)
+  except Exception as e:
+    # Handle other exceptions
+    return 'Error:', str(e)
+
+def loadLoadingWindow():
+  global received
+  global loadingWindow
+  global content_label  
+  received = False
+  
+  loadingWindow = tk.Tk()
+  loadingWindow.title("Self-Closing Window") # Title  
+  loadingWindow.overrideredirect(True) # No buttons on top row  
+  loadingWindow.geometry(f"+{loadingWindow.winfo_pointerx()}+{loadingWindow.winfo_pointery()}") # Move window to current mouse position
+  loadingWindow.attributes("-topmost", True) # Appear on top
+  
+  content_label = tk.Label(loadingWindow, text="Loading...", font=("Arial", 22)) # Text
+  content_label.pack() # Necessary
+  
+  loadingWindow.after(10, load_loading_window) # Infinite loop
+  loadingWindow.mainloop() # Run window
+
+def load_loading_window():
+  global received
+  global content_label
+  loading_texts = ["Loading.", "Loading..", "Loading..."]
+  index = 0
+
+  def update_content():
+    nonlocal index
+    if not received:
+      content_label.config(text=loading_texts[index])
+      index = (index + 1) % len(loading_texts)
+      loadingWindow.after(500, update_content)
+    else:
+      loadingWindow.destroy()
+
+
+  update_content()
+
+def startThreads():
+  print('starting threads...')
+  # thread1 = threading.Thread(target=loadLoadingWindow)
+  thread2 = threading.Thread(target=instantGPT)
+
+  # thread1.start()
+  thread2.start()
+
+keyboard.add_hotkey("ctrl+shift+o", startThreads)
 keyboard.wait('esc')
+
 # watch Date A Live
