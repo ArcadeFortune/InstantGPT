13e1fd472ad62fe5710c9aafd0db2875b7d590e5
ArcadeFortune <alessionights@gmail.com>
2023-05-27 17:36:41 +0200

    fix: no animation but can prompt multiple times

--- InstantGPT.py
+++ InstantGPT.py
@@ -1,10 +1,10 @@
 import os
-import openai
 import keyboard
 import pyautogui
 import pyperclip
 import threading
 import webbrowser
+import openai
 import tkinter as tk
 from dotenv import load_dotenv
 from markdown2 import Markdown
@@ -15,51 +15,63 @@ load_dotenv()
 key = os.getenv('key')
 openai.api_key = key
 
-# Markdown
 markdowner = Markdown()
+text = 'eiusmod tempor incididunt ut labolaborum.'
 
 # Global variables
 previous_text = pyperclip.paste()
 received = False
+highlighted = True
 
-def instantGPT():
+def beautifyLoadingWindow():
+  global myhtmlframe
+  global root
+  global content_label
+  root = tk.Tk()
+  root.overrideredirect(True) # No buttons on top row  
+  root.title("InstantGPT")
+  root.geometry(f"+{root.winfo_pointerx()}+{root.winfo_pointery()}") # Move window to current mouse position
+  root.attributes("-topmost", True) # Appear on top
+  content_label = tk.Label(root, text="Loading...", font=("Arial", 22)) # Text
+  content_label.pack() # Necessary
+  
+  converted = markdowner.convert("# Loading...")
+  myhtmlframe = HtmlFrame(root, horizontal_scrollbar="auto", messages_enabled=False)
+  # myhtmlframe.load_html(converted)
+  # myhtmlframe.pack(fill="both", expand=True)
+  
+def InstantGPT():  
   global previous_text
   global received
   current_text = readHighlightedText()
   
-  root = tk.Tk()
-  
   if current_text: # Error handling
-    if current_text != previous_text:
+    if current_text != previous_text:      
       answer = sendRequest(current_text)
-      if answer:          
-        received = True # Confirm that answer has been received
+      if answer:
+        global myhtmlframe
+        global root
+        global content_label
+        content_label.pack_forget()
+        root.overrideredirect(False) # Buttons on top row  
         converted = markdowner.convert(answer)
-        myhtmlframe = HtmlFrame(root, horizontal_scrollbar="auto", messages_enabled=False)
-        # myhtmlframe.messages_enabled = False
-        myhtmlframe.load_html(converted)
+        myhtmlframe.load_html(converted)  
         myhtmlframe.pack(fill="both", expand=True)
-        root.mainloop()
-        
+                
+        resizeWindow()
       else:
         print("Something went wrong with the API request")
         
     else:
       # No highlighted text, open OpenAI website
+      global highlighted
+      highlighted = False
       print("No highlighted text, open OpenAI website")
       webbrowser.open('https://chat.openai.com')
       
   else: # Error handling
     print("Something went wrong with the highlighted text")
-    
-  previous_text = current_text # Update previous text
-
-def readHighlightedText():
-  pyautogui.hotkey('ctrl', 'c')    
-  current_text = pyperclip.paste()
-  print("highlighted text: " + current_text)
-  return current_text
-
+  
 def sendRequest(current_text):  
   try:      
     answer = ""    
@@ -82,54 +94,23 @@ def sendRequest(current_text):
     # Handle other exceptions
     return 'Error:', str(e)
 
-def loadLoadingWindow():
-  print('loading answer...')
-  global received
-  global loadingWindow
-  global content_label  
-  received = False
-  
-  loadingWindow = tk.Tk()
-  loadingWindow.title("Self-Closing Window") # Title  
-  loadingWindow.overrideredirect(True) # No buttons on top row  
-  loadingWindow.geometry(f"+{loadingWindow.winfo_pointerx()}+{loadingWindow.winfo_pointery()}") # Move window to current mouse position
-  loadingWindow.attributes("-topmost", True) # Appear on top
-  
-  content_label = tk.Label(loadingWindow, text="Loading...", font=("Arial", 22)) # Text
-  content_label.pack() # Necessary
-  
-  loadingWindow.after(10, load_loading_window) # Infinite loop
-  loadingWindow.mainloop() # Run window
+def resizeWindow():
+    frame_width = myhtmlframe.winfo_reqwidth()
+    frame_height = myhtmlframe.winfo_reqheight()
+    root.geometry(f"{frame_width}x{frame_height}")
+    
+def readHighlightedText():
+  pyautogui.hotkey('ctrl', 'c')    
+  current_text = pyperclip.paste()
+  print("highlighted text: " + current_text)
+  return current_text
+      
+def main():
+  global root
+  beautifyLoadingWindow()
+  root.after(50, InstantGPT)
+  root.after(100, readHighlightedText)
+  root.mainloop()
 
-def load_loading_window():
-  global received
-  global content_label
-  loading_texts = ["Loading.", "Loading..", "Loading..."]
-  index = 0
-
-  def update_content():
-    print('loading answer...')
-    nonlocal index
-    if not received:
-      content_label.config(text=loading_texts[index])
-      index = (index + 1) % len(loading_texts)
-      loadingWindow.after(500, update_content)
-    else:
-      print('answer received')
-      loadingWindow.destroy()
-
-
-  update_content()
-
-def startThreads():
-  print('starting threads...')
-  thread1 = threading.Thread(target=loadLoadingWindow)
-  thread2 = threading.Thread(target=instantGPT)
-
-  thread1.start()
-  thread2.start()
-
-keyboard.add_hotkey("ctrl+shift+o", startThreads)
+keyboard.add_hotkey('ctrl+shift+o', main)
 keyboard.wait('esc')
-
-# watch Date A Live
